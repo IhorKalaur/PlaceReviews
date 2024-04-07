5e3b1d84d3428f91c2a6a8bd5c5c98b95af55951
KIM <KIM@d.c>
2024-04-07 14:16:42 +0300

    integrated Google Places library in the project

--- src/main/java/ihor/kalaur/demo/dto/PlaceSearchRequestDto.java
+++ src/main/java/ihor/kalaur/demo/dto/PlaceSearchRequestDto.java
@@ -2,12 +2,13 @@ package ihor.kalaur.demo.dto;
 
 public record PlaceSearchRequestDto(
     String textQuery,
+    Integer maxResultCount,
     LocationBias locationBias
 ) {
     public record LocationBias(Circle circle) {
     }
 
-    public record Circle(Center center, Integer radius) {
+    public record Circle(Center center, Double radius) {
     }
 
     public record Center(Double latitude, Double longitude) {
--- src/main/java/ihor/kalaur/demo/service/impl/GooglePlaceApiServiceImpl.java
+++ src/main/java/ihor/kalaur/demo/service/impl/GooglePlaceApiServiceImpl.java
@@ -1,62 +1,111 @@
 package ihor.kalaur.demo.service.impl;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.places.v1.MapsPlaces;
+import com.google.api.services.places.v1.model.*;
 import ihor.kalaur.demo.dto.PlaceSearchRequestDto;
 import ihor.kalaur.demo.dto.PlaceSearchResponseDto;
 import ihor.kalaur.demo.exceptions.FetchDataFromGoogleApiException;
 import ihor.kalaur.demo.service.GooglePlaceApiService;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import ihor.kalaur.demo.dto.PlaceSearchResponseDto.Place.Review;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class GooglePlaceApiServiceImpl implements GooglePlaceApiService {
-    private static final String GOOGLE_PLACES_API_URL = "https://places.googleapis.com/v1/places:searchText";
-    private static final String CONTENT_TYPE_HEADER = "Content-Type";
     private static final String API_KEY_HEADER = "X-Goog-Api-Key";
-    private static final String FIELD_MASK_HEADER = "X-Goog-FieldMask";
+    private static final String RETURNED_TYPES_FIELD_MASK_HEADER = "X-Goog-FieldMask";
     private static final String FIELD_MASK_VALUE = "places.displayName.text,places.reviews";
-    private static final String APPLICATION_JSON = "application/json";
-    private static final String ERROR_MESSAGE = "Can't get data about restaurant: ";
-
-    private final ObjectMapper objectMapper;
-
     @Value("${ihor.kalaur.demo.google.place.api.key}")
     private String apiKey;
 
-    public PlaceSearchResponseDto fetchRestaurantReviews(PlaceSearchRequestDto searchRequest) {
-        try {
-            HttpRequest request = buildHttpRequest(searchRequest);
-            HttpResponse<String> response = executeHttpRequest(request);
+    private MapsPlaces mapsPlacesService;
 
-            System.out.println("Status Code: " + response.statusCode());
-            System.out.println("Response Body: " + response.body());
-
-            return objectMapper.readValue(response.body(), PlaceSearchResponseDto.class);
-        } catch (Exception e) {
-            throw new FetchDataFromGoogleApiException(ERROR_MESSAGE + searchRequest.textQuery(), e);
-        }
-    }
-
-    private HttpRequest buildHttpRequest(PlaceSearchRequestDto searchRequest) throws Exception {
-        String requestBody = objectMapper.writeValueAsString(searchRequest);
-
-        return HttpRequest.newBuilder()
-                .uri(URI.create(GOOGLE_PLACES_API_URL))
-                .header(CONTENT_TYPE_HEADER, APPLICATION_JSON)
-                .header(API_KEY_HEADER, apiKey)
-                .header(FIELD_MASK_HEADER, FIELD_MASK_VALUE)
-                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+    @PostConstruct
+    public void init() {
+        mapsPlacesService = new MapsPlaces.Builder(new NetHttpTransport(), new GsonFactory(), null)
+                .setApplicationName("Your Application Name")
+                .setGoogleClientRequestInitializer(request -> request.getRequestHeaders()
+                        .set(API_KEY_HEADER, apiKey)
+                        .set(RETURNED_TYPES_FIELD_MASK_HEADER, FIELD_MASK_VALUE))
                 .build();
     }
 
-    private HttpResponse<String> executeHttpRequest(HttpRequest request) throws Exception {
-        HttpClient httpClient = HttpClient.newHttpClient();
-        return httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+    @Override
+    public PlaceSearchResponseDto fetchRestaurantReviews(PlaceSearchRequestDto searchRequestDto) {
+        try {
+            GoogleMapsPlacesV1SearchTextRequest requestModel = convertToGoogleSearchRequest(searchRequestDto);
+            GoogleMapsPlacesV1SearchTextResponse response = mapsPlacesService.places().searchText(requestModel).execute();
+            log.info("Response Body: {}", response);
+            return convertResponseToDto(response);
+        } catch (Exception e) {
+            log.error("Failed to fetch restaurant reviews", e);
+            throw new FetchDataFromGoogleApiException("Can't get data about restaurant: " + searchRequestDto, e);
+        }
+    }
+
+    public GoogleMapsPlacesV1SearchTextRequest convertToGoogleSearchRequest(PlaceSearchRequestDto requestDto) {
+        GoogleMapsPlacesV1SearchTextRequest googleRequest = new GoogleMapsPlacesV1SearchTextRequest();
+        googleRequest.setTextQuery(requestDto.textQuery());
+
+        if (requestDto.locationBias() != null && requestDto.locationBias().circle() != null) {
+            GoogleMapsPlacesV1SearchTextRequestLocationBias locationBias = createLocationBias(requestDto);
+            googleRequest.setLocationBias(locationBias);
+        }
+
+        googleRequest.setMaxResultCount(requestDto.maxResultCount());
+
+        return googleRequest;
+    }
+
+    private GoogleMapsPlacesV1SearchTextRequestLocationBias createLocationBias(PlaceSearchRequestDto requestDto) {
+        GoogleMapsPlacesV1Circle circle = new GoogleMapsPlacesV1Circle();
+        GoogleTypeLatLng center = new GoogleTypeLatLng();
+
+        center.setLatitude(requestDto.locationBias().circle().center().latitude());
+        center.setLongitude(requestDto.locationBias().circle().center().longitude());
+        circle.setCenter(center);
+
+        circle.setRadius(requestDto.locationBias().circle().radius());
+
+        GoogleMapsPlacesV1SearchTextRequestLocationBias locationBias = new GoogleMapsPlacesV1SearchTextRequestLocationBias();
+        locationBias.setCircle(circle);
+        return locationBias;
+    }
+
+    private PlaceSearchResponseDto convertResponseToDto(GoogleMapsPlacesV1SearchTextResponse apiResponse) {
+        List<PlaceSearchResponseDto.Place> places = apiResponse.getPlaces().stream().map(this::convertApiPlaceToDto).collect(Collectors.toList());
+        return new PlaceSearchResponseDto(places);
+    }
+
+
+    private PlaceSearchResponseDto.Place convertApiPlaceToDto(GoogleMapsPlacesV1Place apiPlace) {
+        PlaceSearchResponseDto.Place.DisplayName displayName = new PlaceSearchResponseDto.Place.DisplayName(Optional.ofNullable(apiPlace.getDisplayName()).map(name -> name.getText()).orElse(""));
+
+        List<Review> reviews = Optional.ofNullable(apiPlace.getReviews()).orElse(Collections.emptyList()).stream().map(this::convertApiReviewToDto).collect(Collectors.toList());
+
+        return new PlaceSearchResponseDto.Place(displayName, reviews);
+    }
+
+    private Review convertApiReviewToDto(GoogleMapsPlacesV1Review apiReview) {
+        Review.ReviewText text = Optional.ofNullable(apiReview.getText()).map(t -> new Review.ReviewText(t.getText(), t.getLanguageCode())).orElse(new Review.ReviewText("", ""));
+
+        Review.ReviewText originalText = Optional.ofNullable(apiReview.getOriginalText()).map(ot -> new Review.ReviewText(ot.getText(), ot.getLanguageCode())).orElse(new Review.ReviewText("", ""));
+
+        Review.AuthorAttribution authorAttribution = new Review.AuthorAttribution(Optional.ofNullable(apiReview.getAuthorAttribution()).map(GoogleMapsPlacesV1AuthorAttribution::getDisplayName).orElse(""), Optional.ofNullable(apiReview.getAuthorAttribution()).map(GoogleMapsPlacesV1AuthorAttribution::getUri).orElse(""), Optional.ofNullable(apiReview.getAuthorAttribution()).map(GoogleMapsPlacesV1AuthorAttribution::getPhotoUri).orElse(""));
+
+        return new Review(Optional.ofNullable(apiReview.getName()).orElse(""),
+                Optional.ofNullable(apiReview.getRelativePublishTimeDescription()).orElse(""), Optional.ofNullable(apiReview.getRating()).map(Double::intValue).orElse(0), // Обробка null для числових значень
+                text, originalText, authorAttribution, Optional.ofNullable(apiReview.getPublishTime()).orElse(""));
     }
 }
